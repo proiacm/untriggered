409d946cb0801551390ef7d9c46912327e6a1700
proiacm <proiacm@gmail.com>
2020-06-18 17:27:49 -0600

    add validation to models

--- app/models/emotion.rb
+++ app/models/emotion.rb
@@ -1,4 +1,5 @@
 class Emotion < ApplicationRecord
     has_many :entries 
     has_many :users, through: :entries
+    validates_presence_of :name
 end
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -2,4 +2,5 @@ class Entry < ApplicationRecord
     belongs_to :user 
     belongs_to :emotion
     accepts_nested_attributes_for :emotion, reject_if: :all_blank
+    validates_presence_of :content
 end
