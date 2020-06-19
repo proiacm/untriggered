4931654ae4842de71638d98fa21466084660eae7
proiacm <proiacm@gmail.com>
2020-06-19 14:52:59 -0600

    build scope method

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -3,4 +3,5 @@ class Entry < ApplicationRecord
     belongs_to :emotion
     accepts_nested_attributes_for :emotion, reject_if: :all_blank
     validates_presence_of :content
+    scope :current_week, -> { where('DATE(created_at) >= ?', 7.days.ago) }
 end
